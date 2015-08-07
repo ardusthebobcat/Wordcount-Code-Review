c02c5f4c1e78ae1c519c69434ca9c01301e6bcfd
Michael Summers <student@epicodus.com>
2015-08-07 09:47:33 -0700

    Initializes forgotten integration_spec file, creates integration spec to fill in search string and search term, and checks page for count of search term.

--- lib/wordcount.rb
+++ lib/wordcount.rb
@@ -1,7 +1,8 @@
 class String
   define_method(:wordcount) do |word_search|
-    #define search array, split by word
+    #define search array, splits by word
     #added in downcasecase to prevent failure from capitalization
+    #also intializes word_instances count
     search_string = self.downcase
     search_array = search_string.split(" ")
     word_instances = 0
@@ -12,6 +13,7 @@ class String
     #initialize counter
     i = 0
 
+    #searches entire word for parameter, adds to word_instances
     search_array.each do
       if search_array[i] == word_search
         word_instances += 1
@@ -21,7 +23,7 @@ class String
       i += 1
     end
 
-
+    #Returns count of instances
     return word_instances
   end
 end
