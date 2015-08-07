c876c5e85c456167be897866dd91a4c7377a3150
Michael Summers <student@epicodus.com>
2015-08-07 09:33:58 -0700

    Now searches entire string for parameter word, outputs count of parameter word. Handles crazycase. Contains specs to match, phases out first spec.

--- lib/wordcount.rb
+++ lib/wordcount.rb
@@ -1,8 +1,27 @@
 class String
   define_method(:wordcount) do |word_search|
     #define search array, split by word
-    search_array = self.split(" ")
+    #added in downcasecase to prevent failure from capitalization
+    search_string = self.downcase
+    search_array = search_string.split(" ")
+    word_instances = 0
 
-    return word_search
+    #lets lowercase the search term, just in case
+    word_search = word_search.downcase
+
+    #initialize counter
+    i = 0
+
+    search_array.each do
+      if search_array[i] == word_search
+        word_instances += 1
+      end
+
+      #iterate counter
+      i += 1
+    end
+
+
+    return word_instances
   end
 end
--- spec/wordcount_spec.rb
+++ spec/wordcount_spec.rb
@@ -3,7 +3,15 @@ require('pry')
 require('wordcount')
 
 describe('String#wordcount') do
-  it ("accepts a string and parameter, returns parameter") do
-    expect(("Potato onion tomato onion").wordcount("onion")).to(eq("onion"))
+  # it ("accepts a string and parameter, returns parameter") do
+  #   expect(("Potato onion tomato onion").wordcount("onion")).to(eq("onion"))
+  # end
+  it ("accepts string and param, returns count of param within string") do
+    expect(("Potato onion tomato onion").wordcount("onion")).to(eq(2))
+  end
+
+  #Just to double check that it doesnt get confused, multiple copied words in string
+  it ("accepts a complex string with multiple repeated words, returns count of only param") do
+    expect(("Walrus fish rock BoBCat boBCAT mouse fish bobcat").wordcount("bobcat")).to(eq(3))
   end
 end
